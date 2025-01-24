2b5edf60be67f41312e8d196afb5033a39d82ebd
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-01-24 16:36:53 +0000

    Add PracticalExperience component
    Created the PracticalExperience component
    Added a h2 containing 'Practical Experience'
    Exported and imported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { GeneralInformation } from "./components/GeneralInformation.jsx";
 import { EducationalExperience } from "./components/EducationalExperience.jsx";
+import { PracticalExperience } from "./components/PracticalExperience.jsx";
 
 function App() {
   return (
     <>
       <GeneralInformation />
       <EducationalExperience />
+      <PracticalExperience />
     </>
   );
 }
