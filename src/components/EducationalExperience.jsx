82e11a19ab1e7750531f0f029074e7537974d062
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-01-28 17:19:27 +0000

    Add Three h3
    Added three h3 elements for School name, Title of study and Date of study
    This are to display the output of the users information

--- src/components/EducationalExperience.jsx
+++ src/components/EducationalExperience.jsx
@@ -1,5 +1,12 @@
 function EducationalExperience() {
-  return <h2>Educational Experience</h2>;
+  return (
+    <>
+      <h2>Educational Experience</h2>
+      <h3>School name:</h3>
+      <h3>Title of study:</h3>
+      <h3>Date of study</h3>
+    </>
+  );
 }
 
 export { EducationalExperience };
