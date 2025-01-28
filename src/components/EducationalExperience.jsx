26d637083b17c9b0377a903a5418dca6e5d37e02
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-01-28 17:49:13 +0000

    handleInput function
    Added handleInput function
    Call useState setter function to update a spread version of the educationalExperience state variable
    Use bracket notation to target the fieldName parameter/variable
    call arrow function in onChange attribute that takes in an event
    calls handleInput with its own event and the fieldName label which is set to schoolName

--- src/components/EducationalExperience.jsx
+++ src/components/EducationalExperience.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import { EditButton } from "./EditButton";
 
 function EducationalExperience() {
+  const [educationalExperience, setEducationalExperience] = useState({
+    schoolName: "",
+    studyTitle: "",
+    studyDate: "",
+  });
+
+  const handleInput = (event, fieldName) => {
+    setEducationalExperience((prevEducationalExperience) => ({
+      ...prevEducationalExperience,
+      [fieldName]: event.target.value,
+    }));
+    return generalInformation[fieldName];
+  };
+
   return (
     <>
       <h2>Educational Experience</h2>
-      <h3>School name:</h3>
+      <h3>School name: {educationalExperience.schoolName}</h3>
+      <input
+        type="text"
+        value={educationalExperience.schoolName}
+        onChange={(e) => {
+          handleInput(e, "schoolName");
+        }}
+      />
       <h3>Title of study:</h3>
       <h3>Date of study</h3>
       <EditButton />
