9d0fccb8696d3039b6bc1ef94e12787956bdeac1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2025-01-30 11:18:07 +0000

    import useState Hook
    Imported useState Hook
    set state variable and setter function
    set useState initial value to an object containing compnanyName, positionTitle, responsibilities, employmentDate properties with empty strings as their values

--- src/components/PracticalExperience.jsx
+++ src/components/PracticalExperience.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { SaveButton } from "./SaveButton";
 
 function PracticalExperience() {
+  const [practicalExperience, setPracticalExperience] = useState({
+    companyName: "",
+    positionTitle: "",
+    responsibilities: "",
+    employmentDate: "",
+  });
+
   return (
     <>
       <h2>Practical Experience</h2>
